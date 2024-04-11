9552fc63a55ff81f4dca47b921d4c70c931491bb
Saikat Nandi <saikat.nandi@superiorbusinessresults.com>
2024-04-11 13:14:54 +0700

    Rename type

--- src/types/customer_product_recommendation_type.jl
+++ src/types/customer_product_recommendation_type.jl
@@ -1,2 +1,2 @@
 # tuple of customer, product, recommender name, relative score
-const CustomerProductRecommendation = Tuple{String, String, String, RecoScore}
+const CustomerProductReco = Tuple{String, String, String, RecoScore}
