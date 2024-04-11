00f7f2fb80b6f2313d3aef181911ac34a01cf473
Saikat Nandi <saikat.nandi@superiorbusinessresults.com>
2024-04-11 13:15:38 +0700

    Add conversion method from integer to reco score

--- src/types/recommendation_score_type.jl
+++ src/types/recommendation_score_type.jl
@@ -4,8 +4,13 @@
 struct RecoScore
     reco_score::Int
 
-    function RecoScore(s)
+    # make sure that score is an integer between 0-100
+    # this standard makes it easier to create composite score when multiple recommenders are combined
+    function RecoScore(s::Int)
         @assert 0 <= s <= 100
         return new(s)
     end
 end
+
+# automatically convert integer to RecoScore type
+Base.convert(::Type{RecoScore}, x::Int) = RecoScore(x)
