bae4d93ce1e4748bee638e47840604e25534ff44
Saikat Nandi <saikat.nandi@superiorbusinessresults.com>
2024-04-11 13:32:47 +0700

    Update inline comments

--- src/types/recommendation_score_type.jl
+++ src/types/recommendation_score_type.jl
@@ -1,11 +1,11 @@
 # recommendation score
-# integet restricted to 0-100
 # higher score treated as stronger recommendation
+# integet restricted to 0-100
+# this standard is useful when combining recommendations from multiple recommenders
 struct RecoScore
     reco_score::Int
 
     # make sure that score is an integer between 0-100
-    # this standard makes it easier to create composite score when multiple recommenders are combined
     function RecoScore(s::Int)
         @assert 0 <= s <= 100
         return new(s)
