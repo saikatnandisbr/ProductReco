876ae55af4111ff2f9aaeb1015ad8051909656e7
Saikat Nandi <saikat.nandi@superiorbusinessresults.com>
2024-04-13 14:31:48 +0700

    Update doc string

--- src/types/recommendation_score_type.jl
+++ src/types/recommendation_score_type.jl
@@ -6,11 +6,30 @@ struct RecoScore
     reco_score::Int
 
     # make sure that score is an integer between 0-100
+    """
+        function RecoScore(s::Int)
+        
+    Constuctor for RecoScore user defined type.
+    Check passed integer against valid range. If valid then return constructed object.
+
+    s:  Ingeter score
+    """
+
     function RecoScore(s::Int)
         @assert 0 <= s <= 100
         return new(s)
     end
 end
 
-# automatically convert integer to RecoScore type
+"""
+    function Base.convert(::Type{RecoScore}, x::Int)
+
+Sinhle-line funcition to automatically convert integer to RecoScore type.
+Overload function in Base module.
+Call constructor of RecoScore type with passed integer.
+
+first argument:     RecoScore (user defined type)
+x:                  Integer to be converted to RecoScore type
+"""
+
 Base.convert(::Type{RecoScore}, x::Int) = RecoScore(x)
