3a970d108f7876655599152648ab7b05b888f20b
Saikat Nandi <saikat.nandi@superiorbusinessresults.com>
2024-05-07 14:42:06 +0700

    Export type

--- src/types/customer_product_recommendation_type.jl
+++ src/types/customer_product_recommendation_type.jl
@@ -1,4 +1,7 @@
 # tuple of customer, product, recommender name, relative score
 
+# exports
+export CustomerProductReco
+
 # code
 const CustomerProductReco = Tuple{String, String, String, RecoScore}
