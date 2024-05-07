f4b7ff7dc1f8fbb867817991279d8abec885eeb1
Saikat Nandi <saikat.nandi@superiorbusinessresults.com>
2024-05-07 14:39:37 +0700

    Overload base convert for customer type

--- src/types/customer_type.jl
+++ src/types/customer_type.jl
@@ -1,8 +1,24 @@
 # customer parametric type
 
+# exports
+export Customer
+
 # code
 struct Customer{T <: Union{String, Int}}
 
     customer_id::T
 
 end
+
+"""
+    function Base.convert(::Type{Customer}, customer_id::T) where {T <: Union{String, Int}}
+
+Single-line funcition to automatically convert permissible types to Customer.
+Overload function in Base module.
+Call constructor of Customer type with passed value.
+
+first argument:     Customer (user defined type)
+customer_id:        ID to be converted to Customer type
+"""
+
+Base.convert(::Type{Customer}, customer_id::T) where {T <: Union{String, Int}} = Customer(customer_id)
