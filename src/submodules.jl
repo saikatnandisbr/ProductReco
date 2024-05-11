03d96616802a6eece23b4ae6ec1f4065c9dc5143
saikatnandisbr <saikat.nandi@superiorbusinessresults.com>
2024-05-12 03:24:20 +0700

    Update comments to explain submodule inclusion into parent module

--- src/submodules.jl
+++ src/submodules.jl
@@ -1,5 +1,8 @@
 # submodules
 
 # includes
-include("submodules/pr_coll_filtering/PRCollFiltering.jl")              # PRCollFiltering - collaborative filtering submodule
-using .PRCollFiltering                                                  # import submodule into parent
+# 1. include submodule entry file
+# 2. import submodule into paranet module
+# 3. export from parent module, if desired, submodule objects
+include("submodules/pr_coll_filtering/PRCollFiltering.jl")              # 1. submodule PRCollFiltering - collaborative filtering
+using .PRCollFiltering                                                  # 2. import submodule into parent
