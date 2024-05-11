89e92d38a84fd09f136f09f5ec88f07275168b69
saikatnandisbr <saikat.nandi@superiorbusinessresults.com>
2024-05-12 03:01:39 +0700

    Include new types into module

--- src/types.jl
+++ src/types.jl
@@ -2,6 +2,8 @@
 
 # includes
 include("types/customer_type.jl")                           # customer
+include("types/product_type.jl")                            # product
+include("types/rating_type.jl")                             # rating
 include("types/customer_product_rating_type.jl")            # customer product rating
 include("types/recommendation_score_type.jl")               # recommendation score restricted range
 include("types/customer_product_recommendation_type.jl")    # customer product recommendation
