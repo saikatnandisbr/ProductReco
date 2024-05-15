08aee5694ea5c2928ac7c1c67af457d0378633e8
saikatnandisbr <saikat.nandi@superiorbusinessresults.com>
2024-05-16 00:23:12 +0700

    Make Rating float for linear algebra, define accessor

--- src/types/rating_type.jl
+++ src/types/rating_type.jl
@@ -1,23 +1,40 @@
-# customer product rating parametric type
+# customer product rating type
 
 # exports
 export Rating
+export val
 
 # code
-struct Rating{T <: Union{Int64, Float64}}
+struct Rating
 
-    rating::T
+    rating::Float64                 # float needed for SVD
 
 end
 
 """
-    function Base.convert(::Type{Rating}, rating::T) where {T <: Union{Int64, Float64}}
+    Base.convert(::Type{Rating}, rating::T) where {T <: Union{Int64, Float64}}
 
 Single-line funcition to automatically convert permissible types to Rating.
 Overload function in Base module.
 
 first argument:    Rating (user defined type)
-rating:            Value to be converted to Rating type
+rating:            Value to be converted to rating
 """
 
 Base.convert(::Type{Rating}, rating::T) where {T <: Union{Int64, Float64}} = Rating(rating)
+
+
+"""
+    function val(obj::Rating)
+
+Accessor function to get value.
+
+obj:                Rating object
+"""
+
+function val(obj::Rating)
+
+    return obj.rating
+
+end
+
