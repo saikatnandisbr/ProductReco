199c6c022fef44c385833d4b5d75209eb7dab1e5
Saikat Nandi <saikat.nandi@superiorbusinessresults.com>
2024-05-16 08:22:53 +0700

    Include transform method in contract

--- src/types/recommender_abstract_type.jl
+++ src/types/recommender_abstract_type.jl
@@ -9,25 +9,44 @@ abstract type Recommender end
 
 # contract
 """
-    function fit(recommender::Recommender, agrs...; kwargs...) 
+    function fit!(recommender::Recommender, agrs...; kwargs...)::Recommender 
 
 Fits data to create model that generates product recommendations.
+Returns the fitted Recommender object.
 
 recommnder:     Recommender type object
 agrs:           Tuple of variable number of arguments
 kwargs:         Tuple of variable number of keyword arguments 
 """
 
-function fit!(recommender::Recommender, agrs...; kwargs...) 
+function fit!(recommender::Recommender, agrs...; kwargs...)::Recommender
 
-    error("fit method not defined in the concrete type")
+    error("fit! method not defined in the concrete type")
 
 end
 
 """
+    function transform!(recommender::Recommender, agrs...; kwargs...)::Recommender 
+
+Transforms data, potentially new, using fitted model.
+Returns the transformed Recommender object.
+
+recommnder:     Recommender type object
+agrs:           Tuple of variable number of arguments
+kwargs:         Tuple of variable number of keyword arguments 
+"""
+
+function transform!(recommender::Recommender, agrs...; kwargs...)::Recommender
+
+    error("transform! method not defined in the concrete type")
+
+end
+
+
+"""
     function predict(recommender::Recommender, agrs...; kwargs...)::Vector{CustomerProductRecommendation} 
 
-Returns vector of customer product recommendations (::CustomerProductRecommendation).
+Returns vector of customer product recommendations.
 
 recommnder:     Recommender type object
 agrs:           Tuple of variable number of arguments
