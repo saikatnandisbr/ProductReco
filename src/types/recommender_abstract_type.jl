7879f90df4e7ad864dcffa714dae6aebe1c35664
Saikat Nandi <saikat.nandi@superiorbusinessresults.com>
2024-05-16 10:22:17 +0700

    Define accessors and fit_transform

--- src/types/recommender_abstract_type.jl
+++ src/types/recommender_abstract_type.jl
@@ -9,7 +9,35 @@ abstract type Recommender end
 
 # contract
 """
-    function fit!(recommender::Recommender, agrs...; kwargs...)::Recommender 
+    function isfitted(recommender::Recommender)::Bool
+
+Accessor function to get fitted status.
+
+recommender:    Recommender type opbject
+"""
+
+function isfitted(recommender::Recommender)::Bool
+
+    error("isfitted method not defined in the concrete type")
+
+end
+
+"""
+    function istransformed(recommender::Reccommender)::Bool
+
+Accessor function to get transformed status.
+
+recommender:    Recommender type opbject
+"""
+
+function istransformed(recommender::Recommender)::Bool
+
+    error("istransformed method not defined in the concrete type")
+
+end
+
+"""
+    function fit!(recommender::Recommender, agrs...; kwargs...)::Recommender
 
 Fits data to create model that generates product recommendations.
 Returns the fitted Recommender object.
@@ -26,7 +54,7 @@ function fit!(recommender::Recommender, agrs...; kwargs...)::Recommender
 end
 
 """
-    function transform!(recommender::Recommender, agrs...; kwargs...)::Recommender 
+    function transform!(recommender::Recommender, agrs...; kwargs...)::Recommender
 
 Transforms data, potentially new, using fitted model.
 Returns the transformed Recommender object.
@@ -42,6 +70,22 @@ function transform!(recommender::Recommender, agrs...; kwargs...)::Recommender
 
 end
 
+"""
+    function fit_transform!(recommender::Recommender, agrs...; kwargs...)::Recommender
+
+Fits and then transforms data using fitted model.
+Returns the transformed Recommender object.
+
+recommnder:     Recommender type object
+agrs:           Tuple of variable number of arguments
+kwargs:         Tuple of variable number of keyword arguments 
+"""
+
+function fit_transform!(recommender::Recommender, agrs...; kwargs...)::Recommender
+
+    error("fit_transform! method not defined in the concrete type")
+
+end
 
 """
     function predict(recommender::Recommender, agrs...; kwargs...)::Vector{CustomerProductRecommendation} 
