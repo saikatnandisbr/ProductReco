1ec870a58fe3a87cbaef8ecb97c1e5647d2e7a33
Saikat Nandi <saikat.nandi@superiorbusinessresults.com>
2024-05-19 10:13:20 +0700

    Update comments

--- src/submodules/pr_coll_filtering/types/coll_filtering_svd_type.jl
+++ src/submodules/pr_coll_filtering/types/coll_filtering_svd_type.jl
@@ -307,14 +307,14 @@ function PRCollFiltering.similar_customers(cf::CollFilteringSVD, cust::Customer)
     cust_id = id(cust)                              # customer id
     cust_idx = cf.cust_idx_map[cust_id]             # customer index in sparse array
 
-    # similar customer indices
+    # similar customer index
     similar_idx = cf.similar_cust_pair[cf.similar_cust_pair[:, 1] .== cust_idx, 2]
     similarity = cf.similarity[cf.similar_cust_pair[:, 1] .== cust_idx]      
 
     # reverse lookup customer id from customer index
     similar_cust_id = [key for (key, val) in cf.cust_idx_map if val ∈ similar_idx]
 
-    # map constructore over ids
+    # map customer constructor over ids
     customer = map(Customer, similar_cust_id)
 
     similar_customer = [(customer[i], similarity[i]) for i in 1:length(customer)]
