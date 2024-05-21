4f8a5becd122d4775b7cf475a629ec89853d24a2
saikatnandisbr <saikat.nandi@superiorbusinessresults.com>
2024-05-22 02:39:42 +0700

    Logic when fit and transform products do not match

--- src/submodules/pr_coll_filtering/types/includes/coll_filtering_svd_transform.jl
+++ src/submodules/pr_coll_filtering/types/includes/coll_filtering_svd_transform.jl
@@ -14,8 +14,10 @@ function ProductReco.transform!(recommender::CollFilteringSVD)::CollFilteringSVD
         # set status
         recommender.transformed = false
 
-        # call routine to find similar customers
+        # reduce rating matrix using SVD
         prod_cust_rating = recommender.U' * recommender.prod_cust_rating        # reduce using SVD
+
+        # similairy calculated based on reduced rating matrix, may be different from that calculated on rating matrix
         cust_idx, similar_cust_idx, similarity = top_similar_customers_threaded(cosine_vec, recommender.n_max_similar_cust, prod_cust_rating)
 
         # save similar customers
@@ -55,17 +57,30 @@ function ProductReco.transform!(recommender::CollFilteringSVD, data::Vector{Cust
         prod_vec = id.(getfield.(data, 2))
         rating_vec = val.(getfield.(data, 3))
 
+        # products in transform not in fit
+        fit_prod_id = keys(recommender.prod_idx_map)                                  # product ids in fit data
+        new_prod_id = setdiff(prod_vec, fit_prod_id)                                  # product ids in transform but not in fit data
+
         # only keep ratings for products that appear in fitted data
-        fit_prod_ids = keys(recommender.prod_idx_map)                                                       # product ids in fit data
-        transform_prod_ids = unique(prod_vec)                                                               # product ids in transform data
+        keep_rec = [!in(prod_id, new_prod_id) for prod_id in prod_vec]                # index is false for recrods with new products
 
-        new_prod_ids = transform_prod_ids[[!in(prod_id, fit_prod_ids) for prod_id in transform_prod_ids]]   # product ids in transform but not in fit data
+        cust_vec = cust_vec[keep_rec]
+        prod_vec = prod_vec[keep_rec]
+        rating_vec = rating_vec[keep_rec]
 
-        keep = [!in(prod_id, new_prod_ids) for prod_id in prod_vec]                                         # index is false for recrods with new products
+        # create dummy customer ratings if any fit product missing in transform products
+        missing_prod_id = setdiff(fit_prod_id, prod_vec)
+        if !isempty(missing_prod_id)
+            dummy_customer_id = (eltype(cust_vec) ==  String ? string(typemax(Int) * rand()) : rand() * typemax(Int))
+            recommender.dummy_customer_id = dummy_customer_id   # save in case needede later
 
-        cust_vec = cust_vec[keep]            # keep entries for fit products only
-        prod_vec = prod_vec[keep]            # keep entries for fit products only
-        rating_vec = rating_vec[keep]        # keep entries for fit products only
+            # if dummy customer id exists in real data raise error
+            in(dummy_customer_id, cust_vec) && error("Failed to create dummy customer id for missing fit! products")
+ 
+            append!(cust_vec, fill(dummy_customer_id, length(missing_prod_id)))
+            append!(prod_vec, missing_prod_id)
+            append!(rating_vec, zeros(length(missing_prod_id)))
+        end
 
         # id to index map for customers
         cust_idx_map = id_to_index_map(cust_vec)
@@ -81,8 +96,10 @@ function ProductReco.transform!(recommender::CollFilteringSVD, data::Vector{Cust
         prod_cust_rating = sparse(idx_product, idx_customer, rating_vec)
         recommender.prod_cust_rating = prod_cust_rating
 
-        # call routine to find similar customers
-        prod_cust_rating = recommender.U' * prod_cust_rating        # reduce using SVD
+        # reduce rating matrix using SVD
+        prod_cust_rating = recommender.U' * prod_cust_rating
+
+        # similairy calculated based on reduced rating matrix, may be different from that calculated on rating matrix
         cust_idx, similar_cust_idx, similarity = top_similar_customers_threaded(cosine_vec, recommender.n_max_similar_cust, prod_cust_rating)
 
         # save similar customers
