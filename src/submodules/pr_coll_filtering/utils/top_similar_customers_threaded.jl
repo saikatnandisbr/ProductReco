048d659dcedaf186acccc35036ca54ecd179de75
saikatnandisbr <saikat.nandi@superiorbusinessresults.com>
2024-05-23 03:42:54 +0700

    Remove setindex, use views on arrays

--- src/submodules/pr_coll_filtering/utils/top_similar_customers_threaded.jl
+++ src/submodules/pr_coll_filtering/utils/top_similar_customers_threaded.jl
@@ -5,7 +5,7 @@ using SparseArrays
 using Base.Threads
 
 """
-    function top_similar_customers_threaded(fn::Function, top_n::Int64, cust_prod_rating::T) where {T <: Union{Matrix{Float64}, SparseMatrixCSC}}
+    function top_similar_customers_threaded(fn_sim::Function, top_n::Int64, cust_prod_rating::T) where {T <: Union{Matrix{Float64}, SparseMatrixCSC}}
 
 Returns top_n similar customers for each customer and measure of similarity between the pairs.
 
@@ -14,7 +14,7 @@ top_n:                  Number of most similar customers to find
 cust_prod_rating:       Sparse ratings matrix with columns in rows and products in columns
 """
 
-function top_similar_customers_threaded(fn::Function, top_n::Int64, prod_cust_rating::T) where {T <: Union{Matrix{Float64}, SparseMatrixCSC}}
+function top_similar_customers_threaded(fn_sim::Function, top_n::Int64, prod_cust_rating::T) where {T <: Union{Matrix{Float64}, SparseMatrixCSC}}
 
     n_available_threads = nthreads()                          # number of Threads
     len_data = size(prod_cust_rating, 2)                      # length of data to be split among threads
@@ -64,7 +64,7 @@ function top_similar_customers_threaded(fn::Function, top_n::Int64, prod_cust_ra
                         this_cust_idx == compared_cust_idx && continue
 
                         # similarity measure using function passed
-                        sim_score = fn(view(prod_cust_rating, :, this_cust_idx), view(prod_cust_rating, :, compared_cust_idx))
+                        sim_score = fn_sim(view(prod_cust_rating, :, this_cust_idx), view(prod_cust_rating, :, compared_cust_idx))
 
                         # if this pair more similar than least similar pair in top n list then replace the least similar so far with new pair
                         if sim_score > minimum(top_similarity)
@@ -77,9 +77,13 @@ function top_similar_customers_threaded(fn::Function, top_n::Int64, prod_cust_ra
                     count_similar = length(top_similar[top_similar .!= 0])
 
                     # write loop output to thread array
-                    setindex!(cust_idx[thread_number],         top[top_similar .!= 0],             curr_loc:curr_loc+count_similar-1)
-                    setindex!(similar_cust_idx[thread_number], top_similar[top_similar .!= 0],     curr_loc:curr_loc+count_similar-1)
-                    setindex!(similarity[thread_number],       top_similarity[top_similar .!= 0],  curr_loc:curr_loc+count_similar-1)
+                    cust_idx[thread_number][curr_loc:curr_loc+count_similar-1]         = @view top[top_similar .!= 0]
+                    similar_cust_idx[thread_number][curr_loc:curr_loc+count_similar-1] = @view top_similar[top_similar .!= 0]
+                    similarity[thread_number][curr_loc:curr_loc+count_similar-1]       = @view top_similarity[top_similar .!= 0]
+
+                    # setindex!(cust_idx[thread_number],         top[top_similar .!= 0],             curr_loc:curr_loc+count_similar-1)
+                    # setindex!(similar_cust_idx[thread_number], top_similar[top_similar .!= 0],     curr_loc:curr_loc+count_similar-1)
+                    # setindex!(similarity[thread_number],       top_similarity[top_similar .!= 0],  curr_loc:curr_loc+count_similar-1)
 
                     # update location in array
                     curr_loc += count_similar
