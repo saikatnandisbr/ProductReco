e4474f277512949e117c78dfd27404ce57c08293
saikatnandisbr <saikat.nandi@superiorbusinessresults.com>
2024-05-22 12:48:23 +0700

    Separe import export file

--- src/submodules/pr_coll_filtering/types/coll_filtering_svd_type.jl
+++ src/submodules/pr_coll_filtering/types/coll_filtering_svd_type.jl
@@ -4,20 +4,14 @@
 #   Similar customers are found based on comparison of the simplified representation
 #   Products purchased by similar customers but not purchased by given customer are recommended
 
-# imports
-using SparseArrays      # in standard library
-using Random            # in standard library
-using TSVD
-
-# exports
-
 # includes
-include("includes/coll_filtering_svd_main.jl")          # struct and constructor
-include("includes/coll_filtering_svd_accessors.jl")     # accessors defined in Recommender abstract type
-include("includes/coll_filtering_svd_fit.jl")           # fit! from Recommender abstract type
-include("includes/coll_filtering_svd_transform.jl")     # transform! method from Recommender abstract type - two methods
-include("includes/coll_filtering_svd_fit_transform.jl") # fit_transform! method from Recommender abstract type
-include("includes/coll_filtering_svd_predict.jl")       # predict from Recommender abstract type
-include("includes/coll_filtering_svd_similar_customers.jl")  # similar_customers method from Coll Filtering abstract type
+include("includes/coll_filtering_svd_import_export.jl")     # imports and exports
+include("includes/coll_filtering_svd_main.jl")              # struct and constructor
+include("includes/coll_filtering_svd_accessors.jl")         # accessors defined in Recommender abstract type
+include("includes/coll_filtering_svd_fit.jl")               # fit! from Recommender abstract type
+include("includes/coll_filtering_svd_transform.jl")         # transform! method from Recommender abstract type - two methods
+include("includes/coll_filtering_svd_fit_transform.jl")     # fit_transform! method from Recommender abstract type
+include("includes/coll_filtering_svd_predict.jl")           # predict from Recommender abstract type
+include("includes/coll_filtering_svd_similar_customers.jl") # similar_customers method from Coll Filtering abstract type
 
 # code
---
+++ src/submodules/pr_coll_filtering/types/includes/coll_filtering_svd_import_export.jl
@@ -0,0 +1,10 @@
+# imports and exports
+
+# imports
+using SparseArrays      # in standard library
+using Random            # in standard library
+using TSVD
+using LinearAlgebra
+using StatsBase
+
+# exports
