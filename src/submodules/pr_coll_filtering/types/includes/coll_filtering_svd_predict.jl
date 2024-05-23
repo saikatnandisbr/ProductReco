ee47fccb6ad1c504fb5c3b7f9d92a6c51e70a0d3
Saikat Nandi <saikat.nandi@superiorbusinessresults.com>
2024-05-23 12:19:12 +0700

    Replace tuple with individual vectors

--- src/submodules/pr_coll_filtering/types/includes/coll_filtering_svd_predict.jl
+++ src/submodules/pr_coll_filtering/types/includes/coll_filtering_svd_predict.jl
@@ -42,7 +42,10 @@ function prod_from_similar_cust(recommender::CollFilteringSVD, predict_cust_idx:
 
     # accumulator for candidate products
     # (similar_cust_idx, similarity, prod_idx, prod_rating)
-    candidate_prod = Tuple((Vector{Int64}(), Vector{Float64}(), Vector{Int64}(), Vector{Float64}()))
+    candidate_similar_cust_idx = Vector{Int64}()
+    candidate_similarity = Vector{Float64}()
+    candidate_prod_idx = Vector{Int64}()
+    candidate_prod_rating = Vector{Float64}()
 
     # products of predict customer - to be excluded from candidate output
     predict_cust_prod_idx = findnz(recommender.prod_cust_rating[:, predict_cust_idx])[1]
@@ -65,16 +68,16 @@ function prod_from_similar_cust(recommender::CollFilteringSVD, predict_cust_idx:
             in(curr_similar_cust_prod_idx, predict_cust_prod_idx) && continue
 
             # accumulate new products
-            push!(getfield(candidate_prod, 1), curr_similar_cust_idx)
-            push!(getfield(candidate_prod, 2), similarity[i])
-            push!(getfield(candidate_prod, 3), curr_similar_cust_prod_idx)
-            push!(getfield(candidate_prod, 4), similar_cust_prod_rating[j])
+            push!(candidate_similar_cust_idx, curr_similar_cust_idx)
+            push!(candidate_similarity, similarity[i])
+            push!(candidate_prod_idx, curr_similar_cust_prod_idx)
+            push!(candidate_prod_rating, similar_cust_prod_rating[j])
 
         end  # end loop for products of similar customers
         
     end  # end loop for similar customers 
 
-    return candidate_prod
+    return candidate_similar_cust_idx, candidate_similarity, candidate_prod_idx, candidate_prod_rating
 end
 
 """
@@ -91,37 +94,38 @@ function prod_reco_for_customer(recommender::CollFilteringSVD, predict_cust_idx:
 
     # accumulate product recommendations for given customer
     # (prod_idx, raw score)
-    cust_prod_reco = Tuple((Vector{Int64}(), Vector{Float64}()))
+    cust_prod_reco_prod_idx = Vector{Int64}()
+    cust_prod_reco_raw_score = Vector{Float64}()
 
     # candidates for recommendation - products rated by similar customers
-    candidate_prod = prod_from_similar_cust(recommender, predict_cust_idx)
+    candidate_similar_cust_idx, candidate_similarity, candidate_prod_idx, candidate_prod_rating = prod_from_similar_cust(recommender, predict_cust_idx)
 
     # score candidate products
-    for curr_candidate_prod_idx in unique(getfield(candidate_prod, 3))
+    for curr_candidate_prod_idx in unique(candidate_prod_idx)
     
         # slice similar customer similarity rating records by product
-        prod_slice = getfield(candidate_prod, 3) .== curr_candidate_prod_idx         
+        prod_slice = candidate_prod_idx .== curr_candidate_prod_idx         
 
         # get customer similarity and customer rating for the product
-        similarity = @view getfield(candidate_prod, 2)[prod_slice]
-        rating =     @view getfield(candidate_prod, 4)[prod_slice]
+        similarity = @view candidate_similarity[prod_slice]
+        rating =     @view candidate_prod_rating[prod_slice]
 
         score = round(fn_score(similarity, rating), digits=4)
     
         # add to accumulator
-        append!(getfield(cust_prod_reco, 1), curr_candidate_prod_idx)
-        append!(getfield(cust_prod_reco, 2), score)    
+        append!(cust_prod_reco_prod_idx, curr_candidate_prod_idx)
+        append!(cust_prod_reco_raw_score, score)    
     end
 
     # retain top score products
-    n_reco = min(recommender.n_max_reco_per_cust, length(getfield(cust_prod_reco, 1)))   # number of recommendations for predict customer
+    n_reco = min(recommender.n_max_reco_per_cust, length(cust_prod_reco_prod_idx))       # number of recommendations for predict customer
         
-    sort_seq = sortperm(getfield(cust_prod_reco, 2), rev=true)                           # sort by score descending
+    sort_seq = sortperm(cust_prod_reco_raw_score, rev=true)                              # sort by score descending
     
-    top_prod_idx =   @view (@view getfield(cust_prod_reco, 1)[sort_seq])[1:n_reco]       # top products
-    top_prod_score = @view (@view getfield(cust_prod_reco, 2)[sort_seq])[1:n_reco]       # top scores
+    top_prod_idx =   @view (@view cust_prod_reco_prod_idx[sort_seq])[1:n_reco]           # top products
+    top_prod_score = @view (@view cust_prod_reco_raw_score[sort_seq])[1:n_reco]          # top scores
 
-    return (top_prod_idx, top_prod_score)
+    return top_prod_idx, top_prod_score
 
 end
 
@@ -142,9 +146,11 @@ function ProductReco.predict(recommender::CollFilteringSVD, customer::Vector{Cus
     # check error in call to predict
     check_error_in_predict_call(recommender, customer)
 
-    # allocate array to used to accumulate product recommendations
+    # accumulate product recommendations
     # (cust_idx, prod_idx, raw score)
-    prod_reco = Tuple((Vector{Int64}(), Vector{Int64}(), Vector{Float64}()))
+    prod_reco_cust_idx = Vector{Int64}()
+    prod_reco_prod_idx = Vector{Int64}()
+    prod_reco_raw_score = Vector{Float64}()
 
     # customer indices from ids
     predict_cust_id = id.(customer)   
@@ -154,32 +160,31 @@ function ProductReco.predict(recommender::CollFilteringSVD, customer::Vector{Cus
     for (i, curr_predict_cust_idx) in enumerate(predict_cust_idx)
 
         # generate reco
-        cust_prod_reco = prod_reco_for_customer(recommender, curr_predict_cust_idx, fn_score)
+        top_prod_idx, top_prod_score = prod_reco_for_customer(recommender, curr_predict_cust_idx, fn_score)
 
         # add to accumulator
-        n_reco_for_customer = length(getfield(cust_prod_reco, 1))
+        n_reco_for_customer = length(top_prod_idx)
 
-        append!(getfield(prod_reco, 1), fill(curr_predict_cust_idx, n_reco_for_customer))
-        append!(getfield(prod_reco, 2), getfield(cust_prod_reco, 1))
-        append!(getfield(prod_reco, 3), getfield(cust_prod_reco, 2))
+        append!(prod_reco_cust_idx, fill(curr_predict_cust_idx, n_reco_for_customer))
+        append!(prod_reco_prod_idx, top_prod_idx)
+        append!(prod_reco_raw_score, top_prod_score)
 
     end
 
     # convert raw score to relative score
-    n_reco = length(getfield(prod_reco, 1))
+    n_reco = length(prod_reco_cust_idx)
     if n_reco <= 2
         relative_score = fill(100, n_reco)
     else
-        raw_score = getfield(prod_reco, 3)                                              # raw scores across all generated recommendations
-        relative_score = [ceil(Int, percentilerank(raw_score, s)) for s in raw_score]   # relative scores across all generated recommendations
+        relative_score = [ceil(Int, percentilerank(prod_reco_raw_score, s)) for s in prod_reco_raw_score]
     end
 
     # convert idx to id
     idx_cust_map = Dict(values(recommender.cust_idx_map) .=> keys(recommender.cust_idx_map))
-    reco_cust_id = [idx_cust_map[key] for key in getfield(prod_reco, 1)]
+    reco_cust_id = [idx_cust_map[key] for key in prod_reco_cust_idx]
     
     idx_prod_map = Dict(values(recommender.prod_idx_map) .=> keys(recommender.prod_idx_map))
-    reco_prod_id = [idx_prod_map[key] for key in getfield(prod_reco, 2)]
+    reco_prod_id = [idx_prod_map[key] for key in prod_reco_prod_idx]
 
     # return tuple
     return collect(zip(reco_cust_id, reco_prod_id, relative_score))
