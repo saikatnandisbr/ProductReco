54bf6583907777c4aec0ad1432c8ab8101e29c5d
Saikat Nandi <saikat.nandi@superiorbusinessresults.com>
2024-05-25 16:22:10 +0700

    Put back matrix implementation for predict

--- src/submodules/pr_coll_filtering/types/coll_filtering_svd_type.jl
+++ src/submodules/pr_coll_filtering/types/coll_filtering_svd_type.jl
@@ -11,8 +11,8 @@ include("includes/coll_filtering_svd_accessors.jl")         # accessors defined 
 include("includes/coll_filtering_svd_fit.jl")               # fit! from Recommender abstract type
 include("includes/coll_filtering_svd_transform.jl")         # transform! method from Recommender abstract type - two methods
 include("includes/coll_filtering_svd_fit_transform.jl")     # fit_transform! method from Recommender abstract type
-include("includes/coll_filtering_svd_predict.jl")           # predict from Recommender abstract type - database like implementation
-#include("includes/coll_filtering_svd_predict_matrix.jl")   # predict from Recommender abstract type - matrix implementation
+#include("includes/coll_filtering_svd_predict.jl")          # predict from Recommender abstract type - database like implementation
+include("includes/coll_filtering_svd_predict_matrix.jl")    # predict from Recommender abstract type - matrix implementation
 include("includes/coll_filtering_svd_similar_customers.jl") # similar_customers method from Coll Filtering abstract type
 
 # code
