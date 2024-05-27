9a6e4b2f084d830afd804e2174afbade79860039
Saikat Nandi <saikat.nandi@superiorbusinessresults.com>
2024-05-27 08:34:24 +0700

    Add multi-threading

--- src/submodules/pr_coll_filtering/types/includes/coll_filtering_svd_predict.jl
+++ src/submodules/pr_coll_filtering/types/includes/coll_filtering_svd_predict.jl
@@ -53,7 +53,7 @@ function top_prod_for_cust(recommender::CollFilteringSVD, cust_idx::Vector{Int64
     end
 
     # number of max reco per customer
-    n_reco = min(length(recommender.prod_idx_map), recommender.n_max_reco_per_cust) 
+    n_reco_per_cust = min(length(recommender.prod_idx_map), recommender.n_max_reco_per_cust) 
 
     # pre-allocate vectors to store prod recs for each customer
     prod_idx_vec = collect(1:length(recommender.prod_idx_map))
@@ -77,9 +77,9 @@ function top_prod_for_cust(recommender::CollFilteringSVD, cust_idx::Vector{Int64
         (length(prod_idx_nz_vec) == 0) && continue
 
         # retain top products for customer
-        if length(prod_idx_nz_vec) > n_reco
+        if length(prod_idx_nz_vec) > n_reco_per_cust
 
-            top_slice = sortperm(prod_score_nz_vec, rev=true)[1:n_reco]
+            top_slice = sortperm(prod_score_nz_vec, rev=true)[1:n_reco_per_cust]
             prod_idx_top_vec = @view prod_idx_nz_vec[top_slice]
             prod_score_top_vec = @view prod_score_nz_vec[top_slice]
 
@@ -121,43 +121,86 @@ function ProductReco.predict(recommender::CollFilteringSVD, predict_cust::Vector
     # check error in call to predict
     check_error_in_predict_call(recommender, predict_cust)
 
-    # pre-allocate collection to accumate recommendations
-    n_reco = min(length(recommender.prod_idx_map), recommender.n_max_reco_per_cust)    # recommendations per customer
-    max_len = length(predict_cust) * recommender.n_max_reco_per_cust                   # max length of array
+    # set up for multi-threading
+    n_available_threads = nthreads()                          # number of Threads
+    len_data = length(predict_cust)                           # number of customers
+    chunk_size = ceil(Int, len_data / n_available_threads)    # chunk size to be handled by each thread
 
-    prod_reco = (
-        nrow = Vector{Int64}(undef, 1),                                                # counter of recos stored
+    # indices of data to be handled by each thread
+    thread_idx = [collect(i:min(i + chunk_size - 1, len_data)) for i in 1:chunk_size:len_data]
+
+    # different from number of available threads for very small data size
+    n_threads = length(thread_idx)
+
+    # pre-allocate collection to accumate recommendations from each thread
+    n_reco_per_cust = min(length(recommender.prod_idx_map), recommender.n_max_reco_per_cust)        # recommendations per customer
+    max_len = length(predict_cust) * n_reco_per_cust                                                # max length for output vectors
+    max_len_per_thread = ceil(Int, max_len / n_threads)                                             # max length per thread
+
+    # define array of named tuples, one tuple for each thread
+    prod_reco = [
+            (
+            nrow = Vector{Int64}(undef, 1),
+            cust_idx=Vector{Int64}(undef, max_len_per_thread), 
+            prod_idx=Vector{Int64}(undef, max_len_per_thread), 
+            raw_score=Vector{Float64}(undef, max_len_per_thread)
+            )
+        for i in 1:n_threads]
+
+    # initialize record count for each thread
+    for i in 1:n_threads
+        prod_reco[i][:nrow][1] = 0
+    end
+
+    # combined output from all threads
+    prod_reco_combined = (
+        nrow = Vector{Int64}(undef, 1),
         cust_idx=Vector{Int64}(undef, max_len), 
         prod_idx=Vector{Int64}(undef, max_len), 
         raw_score=Vector{Float64}(undef, max_len)
     )
-    
-    prod_reco[:nrow][1] = 0     # initialize record count
 
     # customer indices
     cust_id = id.(predict_cust)
     cust_idx = [recommender.cust_idx_map[id] for id in cust_id]
 
     # generate top product recommendations for customers
-    top_prod_for_cust(recommender, cust_idx, prod_reco, fn_score)
+    @sync begin
+        
+        for thread_num in 1:n_threads
+
+            Threads.@spawn begin
+
+                # separate slice of pro_reco for each thread to populate
+                top_prod_for_cust(recommender, cust_idx[thread_idx[thread_num]], prod_reco[thread_num], fn_score)
+
+            end  # end @spawn
+
+        end  # end loop for threads
+
+    end  # end @sync
+
+    # combine outputs from threads
+    nrow = prod_reco_combined[:nrow][1] = sum([prod_reco[i][:nrow][1] for i in 1:n_threads])
+
+    prod_reco_combined[:cust_idx][1:nrow] = vcat([prod_reco[i][:cust_idx][1:prod_reco[i][:nrow][1]] for i in 1:n_threads]...)
+    prod_reco_combined[:prod_idx][1:nrow] = vcat([prod_reco[i][:prod_idx][1:prod_reco[i][:nrow][1]] for i in 1:n_threads]...)
+    prod_reco_combined[:raw_score][1:nrow] = vcat([prod_reco[i][:raw_score][1:prod_reco[i][:nrow][1]] for i in 1:n_threads]...)
 
     # convert raw score to relative score
-    nrow = prod_reco[:nrow][1]
-
-    # percentile rank requires at least two records
-    if nrow < 2
+    if nrow < 2                                 # percentile rank requires at least two records
         relative_score = fill(100, nrow)
     else
-        raw_score = prod_reco[:raw_score][1:nrow]
+        raw_score = prod_reco_combined[:raw_score][1:nrow]
         relative_score = [ceil(Int, percentilerank(raw_score, s)) for s in raw_score]
     end
 
     # convert idx to id
     idx_cust_map = Dict(values(recommender.cust_idx_map) .=> keys(recommender.cust_idx_map))
-    reco_cust_id = [idx_cust_map[key] for key in prod_reco[:cust_idx][1:nrow]]
+    reco_cust_id = [idx_cust_map[key] for key in prod_reco_combined[:cust_idx][1:nrow]]
     
     idx_prod_map = Dict(values(recommender.prod_idx_map) .=> keys(recommender.prod_idx_map))
-    reco_prod_id = [idx_prod_map[key] for key in prod_reco[:prod_idx][1:nrow]]
+    reco_prod_id = [idx_prod_map[key] for key in prod_reco_combined[:prod_idx][1:nrow]]
 
     # return tuple
     return collect(zip(reco_cust_id, reco_prod_id, relative_score))
